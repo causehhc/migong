f1ee1f7e664d8c05b25baa245a9e49ce93b853c8
CauseHhc <47687676+causehhc@users.noreply.github.com>
2019-06-18 23:43:55 +0800

    取消公测版

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,42 +2,67 @@ package test;
 
 import java.util.Scanner;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+//extends Application
 public class Main {
+//    @Override
+//    public void start(Stage primaryStage) {
+//    	GoMap map=new GoMap();
+//    	int i=247;
+//		int j=477;
+//    	
+//		map.setMap(i, j);
+////		map.stack();
+////		map.queue();
+//		map.setPoint(2, 2, i-3, j-3);
+//		map.runBack();
+//		
+//        primaryStage.setTitle("Title");
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 1280, 720, Color.WHITE);
+//        
+//        for(int i1=0;i1<i*2;i1+=2) {
+//        	for(int j1=0;j1<j*2;j1+=2) {
+//        		Rectangle r = new Rectangle();
+//	        	r.setX(j1*2);
+//			    r.setY(i1*2);
+//		    	r.setWidth(4);
+//			    r.setHeight(4);	
+//			    if(map.trans()[i1/2][j1/2]==0) {
+//			    	r.setFill(Color.BLACK);
+//			    }
+//			    else if(map.trans()[i1/2][j1/2]==8){
+//			    	r.setFill(Color.WHITE);
+//			    }
+//			    else {
+//			    	r.setFill(Color.RED);
+//			    }
+//			    root.getChildren().add(r);
+//        	}
+//        	
+//        }
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
 	
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		GoMap map=new GoMap();
-//		if(in.nextLine().equals("0")) {
-			System.out.println("测试模式");
-			int i=17;
-			int j=67;
-			map.setMap(i, j);
-//			map.stack();
-//			map.recur();
-//			map.queue();
-			
-			map.setPoint(2, 2,i-3,j-3);
-//			map.runBack();
-//			map.runStack();
-//		}
-//		else{
-//			System.out.println("地图大小: (x,y奇数) （空格分隔）");
-//			map.setMap(in.nextInt(), in.nextInt());
-//			System.out.println();
-//			map.print();
-//			System.out.println("生成方法: 1.深度优先（栈） 2.深度优先（递归） 3.广度优先（队列)");
-//			int s=in.nextInt();
-//			switch(s) {
-//				case 1:map.stack();break;
-//				case 2:map.recur();break;
-//				case 3:map.queue();break;
-//				default:System.out.println("fuck me"+in.nextLine()); System.exit(0);
-//			}
-//			map.print();
-//			System.out.println("选择入点出点: （先纵后横）（前面n个红色0就加10*n）（空格分隔）");
-//			map.setPoint(in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt());
-//			System.out.println();
-//			map.runMap();
-//		}
+		int i=17;
+		int j=67;
+		map.setMap(i, j);
+		map.stack();
+//		map.recur();
+//		map.queue();
+		
+		map.setPoint(2, 2,i-3,j-3);
+//		map.runBack();
+		map.runStack();
 	}
 }
