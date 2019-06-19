2505227860a4d6d9939d73db7a91405091f8ea94
CauseHhc <47687676+causehhc@users.noreply.github.com>
2019-06-19 23:58:39 +0800

    实现自动化

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,39 +5,46 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.Scanner;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 
 //extends Application
 
 public class Main extends Application {
+	/**
+	 * f=1:697,1497
+	 * f=2:247,473
+	 * f=3:109,209
+	 */
 	int k = 0, f = 2;
-
+	Timeline action;
 	@Override
 	public void start(Stage primaryStage) {
 		Scanner in=new Scanner(System.in);
 		Pane root = new Pane();
 		GoMap map = new GoMap();
-		int x = 37;
-		int y = 107;
+		int x = 47;
+		int y = 73;
 		map.setMap(x, y);
-		map.setPoint(27, 6, x-3, y-3);
-//		map.stack();
-		map.queue();
-//		map.runStack();
-		map.runStackNY();
+		map.setPoint(2, 2, x-3, y-3);
+		map.stack();
+//		map.queue();
+		map.runStack();
+//		map.runStackNY();
 //		map.runBack();
-		Object[] aimArray = map.queue.toArray().clone();
-
+		
 		for (int i1 = 0; i1 < x * f; i1 += f) {
 			for (int j1 = 0; j1 < y * f; j1 += f) {
 				Rectangle r = new Rectangle();
 				r.setX(j1 * f);
 				r.setY(i1 * f);
-				r.setWidth(2 * f);
-				r.setHeight(2 * f);
+				r.setWidth(f * f);
+				r.setHeight(f * f);
 				if (map.trans()[i1 / f][j1 / f] == 0) {
 					r.setFill(Color.BLACK);
 				} else {
@@ -49,32 +56,31 @@ public class Main extends Application {
 		System.out.println("Calculate complete!");
 
 		Scene scene = new Scene(root, 1900, 1000, Color.WHITE);
-		root.setOnKeyPressed(e -> {
-
-			switch (e.getCode()) {
-			case ENTER:
+		
+		//зп
+		Object[] aimArray = map.queue.toArray().clone();
+		action = new Timeline(new KeyFrame(Duration.millis(1), e -> {
 				if (k >= aimArray.length) {
 					try {
-						
-						Thread.sleep(1000);
-						map.count();
-						System.out.println("OK!");
-						System.exit(0);
+						Thread.sleep(5000);
 					} catch (InterruptedException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
+					map.count();
+					System.out.println("OK!");
+					System.exit(0);
 				}
 				int n = 0;
 				while (n++ < 1) {
 					Rectangle r = new Rectangle();
-					int y1 = ((int) (aimArray[k++]) * f * f);
-					int x1 = ((int) (aimArray[k++]) * f * f);
+					int y1 = ((int) (aimArray[k++]) * f*f );
+					int x1 = ((int) (aimArray[k++]) * f*f);
 //					System.out.println(y1 + " " + x1);
 					r.setX(x1);
 					r.setY(y1);
-					r.setWidth(2* f);
-					r.setHeight(2* f);
+					r.setWidth(f*f);
+					r.setHeight(f*f);
 					if (map.trans()[y1 / (f*f)][x1 / (f*f)] == 1) {
 						r.setFill(Color.RED);
 					} else {
@@ -83,13 +89,9 @@ public class Main extends Application {
 					root.getChildren().add(r);
 
 				}
-				break;
-
-			default:
-				break;
-			}
-
-		});
+		}));
+		action.setCycleCount(Timeline.INDEFINITE);
+		action.play();
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
