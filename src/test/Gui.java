af7dc023bab6e0bf744a4810c8ca0b6c05eb72f1
CauseHhc <47687676+causehhc@users.noreply.github.com>
2019-06-22 00:47:51 +0800

    更新自定义长宽，双屏显示

--- src/test/Gui.java
+++ src/test/Gui.java
@@ -1,6 +1,11 @@
 package test;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -10,118 +15,204 @@ import java.util.Scanner;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.value.ObservableIntegerValue;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 
 //extends Application
 
-public class Gui extends Application {
-
-	/**
-	 * f=1:697,1497 f=2:247,473 f=3:109,209
-	 */
-	int k = 0, num = 0,f = 1;//数组队列下标，开始长度，放大倍率
-	Timeline action;
-	@Override
-	public void start(Stage primaryStage) {
-		Scanner in = new Scanner(System.in);
-		Pane root = new Pane();
-		GoMap map = new GoMap();
-		int[] rdm = new int[4];
-		int x = 247;
-		int y = 473;
-		rdm[0] = (int) (Math.random() * (x - 5)) + 2;
-		rdm[2] = (int) (Math.random() * (x - 5)) + 2;
-		rdm[1] = (int) (Math.random() * (y - 5)) + 2;
-		rdm[3] = (int) (Math.random() * (y - 5)) + 2;
-		/**
-		 ** 1.stack 2.queue 3.recur 1.stack 2.staNY 3.back beginX beginY endX endY
-		 */
-		map.setMap(x, y);
-//		map.choice(1, 1, 2, 2, x - 3, y - 3);
-//		map.choice(1, 1, x - 3, y - 3, 2, 2);
-		map.choice(2, 1, rdm[0], rdm[1], rdm[2], rdm[3]);
-		System.out.println("Calculate complete!");
-
-		for (int i1 = 0; i1 < x * f; i1 += f) {
-			for (int j1 = 0; j1 < y * f; j1 += f) {
-				Rectangle r = new Rectangle();
-				r.setX(j1 * f);
-				r.setY(i1 * f);
-				r.setWidth(f * f);
-				r.setHeight(f * f);
-				if (map.trans()[i1 / f][j1 / f] == 0) {
-					r.setFill(Color.BLACK);
-				} else {
-					r.setFill(Color.WHITE);
-				}
-				root.getChildren().add(r);
-			}
-		}
-		System.out.println("Creat complete!");
-
-		Scene scene = new Scene(root, 1900, 1000, Color.WHITE);
-
-		// 走
-		Object[] aimArray = map.queue.toArray().clone();
-		action = new Timeline(new KeyFrame(Duration.millis(1), e -> {
-			if (k >= aimArray.length) {
-				k=aimArray.length-4;
-				root.setOnKeyPressed(e1 -> {
-					switch (e1.getCode()) {
-					case ENTER:{
-						map.count();
-						System.out.println("OK!");
-//						System.exit(0);
-						break;	
-					}
-					default:
-						break;
-					}
-				});
-			}
-			int n = 0;
-			while (n++ < 1) {
-				Rectangle r = new Rectangle();
-				int y1 = ((int) (aimArray[k++]) * f * f);
-				int x1 = ((int) (aimArray[k++]) * f * f);
-				r.setX(x1);
-				r.setY(y1);
-				r.setWidth(f * f);
-				r.setHeight(f * f);
-				if (map.trans()[y1 / (f * f)][x1 / (f * f)] == 1) {
-					if (num <= 20) {
-						num++;
-						r.setFill(Color.CHARTREUSE);
-					} else if (k >= aimArray.length - 60) {
-						r.setFill(Color.CHARTREUSE);
-					} else {
-						r.setFill(Color.RED);
-					}
-				} else {
-					r.setFill(Color.DARKGRAY);
-				}
-				root.getChildren().add(r);
-
-			}
-		}));
-		action.setCycleCount(Timeline.INDEFINITE);
-		action.play();
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		root.requestFocus();
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-//		Scanner in = new Scanner(System.in);
-//		GoMap map = new GoMap();
-//		int i = 27;
-//		int j = 97;
-//		map.setMap(i, j);
-//		/**
-//		 * 1.stack 2.queue 3.recur 1.stack 2.staNY 3.back beginX beginY endX endY
-//		 */
-//		map.choice(1, 1,2,2,i-3,j-3);
-//		map.print();
-	}
-}
+//public class Main extends Application {
+//Scanner in = new Scanner(System.in);
+//GoMap map = new GoMap();
+///**
+// * f=1:697,1497 f=2:247,473 f=3:109,209 1.stack 2.queue 3.recur 1.stack 2.staNY
+// * 3.back beginX beginY endX endY
+// */
+//int k = 0, f = 2, numOfRt = 0;// 数组队列下标，放大倍率
+//Timeline action;
+//int[] rdm = new int[4];
+//int x = 37;
+//int y = 73;
+//int xs = 0;
+//int ys = 50;
+//
+//void draw(Pane root) {
+//	numOfRt = 0;
+//	for (int i = 0; i < x * f; i += f) {
+//		for (int j = 0; j < y * f; j += f) {
+//			Rectangle r = new Rectangle();
+//			r.setX(j * f + xs);
+//			r.setY(i * f + ys);
+//			r.setWidth(f * f);
+//			r.setHeight(f * f);
+//			if (map.trans()[i / f][j / f] == 0) {
+//				r.setFill(Color.BLACK);
+//			} else {
+//				r.setFill(Color.WHITE);
+//			}
+//			root.getChildren().add(r);
+//			numOfRt++;
+//		}
+//	}
+//}
+//
+//void remove(Pane root) {
+//	root.getChildren().removeAll();
+//}
+//
+//@Override
+//public void start(Stage primaryStage) {
+//	Pane root = new Pane();
+//	
+//	
+//	Button btn1 = new Button("FRESH");
+//	Button btn2 = new Button("START");
+//	Button btn3 = new Button("STOP");
+//	ChoiceBox<String> cb1 = new ChoiceBox<String>(FXCollections.observableArrayList("深度优先算法", "广度优先算法", "回溯法"));
+//	ChoiceBox<String> cb2 = new ChoiceBox<String>(FXCollections.observableArrayList("深度优先算法优化版", "深度优先算法", "回溯法"));
+//
+//	rdm[0] = (int) (Math.random() * (x - 5)) + 2;
+//	rdm[2] = (int) (Math.random() * (x - 5)) + 2;
+//	rdm[1] = (int) (Math.random() * (y - 5)) + 2;
+//	rdm[3] = (int) (Math.random() * (y - 5)) + 2;
+//
+//	map.setPoint(2, 2, x - 3, y - 3);
+//
+//	cb1.setLayoutX(0);
+//	cb2.setLayoutX(150);
+//	btn1.setLayoutX(350);
+//	btn2.setLayoutX(570);
+//	btn3.setLayoutX(650);
+//	root.getChildren().add(cb1);
+//	root.getChildren().add(cb2);
+//	root.getChildren().add(btn1);
+//	root.getChildren().add(btn2);
+//	root.getChildren().add(btn3);
+//	Scene scene = new Scene(root, 1900, 1000, Color.WHITE);//
+//
+//	cb1.getSelectionModel().selectedIndexProperty()
+//			.addListener((ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+//				switch (new_val.intValue()) {
+//				case 0: {
+//					this.remove(root);
+//					map.setMap(x, y);
+//					map.stack();
+//					this.draw(root);
+//					break;
+//				}
+//				case 1: {
+//					this.remove(root);
+//					map.setMap(x, y);
+//					map.queue();
+//					this.draw(root);
+//					break;
+//				}
+//				case 2: {
+//					this.remove(root);
+//					map.setMap(x, y);
+//					try {
+//						map.recur();
+//					} catch (StackOverflowError e) {
+//						System.out.println("StackOverflow");
+//						break;
+//					}
+//					this.draw(root);
+//					break;
+//				}
+//				default:
+//					System.out.println("FuckYou");
+//				}
+//
+//			});
+//
+//	cb2.getSelectionModel().selectedIndexProperty()
+//			.addListener((ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+//				switch (new_val.intValue()) {
+//				case 0: {
+//					map.runStack();
+//					break;
+//				}
+//				case 1: {
+//					map.runStackNY();
+//					break;
+//				}
+//				case 2: {
+//					try {
+//						map.runBack();
+//					} catch (StackOverflowError e) {
+//						System.out.println("StackOverflow");
+//						break;
+//					}
+//					break;
+//				}
+//				default:
+//					System.out.println("FuckYou");
+//				}
+//			});
+//
+//	btn1.setOnAction(e -> {
+//		this.remove(root);
+//		this.draw(root);
+//	});
+//	btn2.setOnAction(e -> {
+//		this.remove(root);
+//		this.draw(root);
+//		this.draw(root);
+//		// 走
+//		k = 0;
+//		Object[] aimArray = map.queue.toArray().clone();
+//		action = new Timeline(new KeyFrame(Duration.millis(1), e1 -> {
+//			if (k >= aimArray.length) {
+//				k = 0;
+//				action.stop();
+//			}
+//			int n = 0;
+//			while (n++ < 1) {
+//				Rectangle r = new Rectangle();
+//				int y1 = ((int) (aimArray[k++]) * f * f);
+//				int x1 = ((int) (aimArray[k++]) * f * f);
+//				r.setX(x1 + xs);
+//				r.setY(y1 + ys);
+//				r.setWidth(f * f);
+//				r.setHeight(f * f);
+//				if (map.trans()[y1 / (f * f)][x1 / (f * f)] == 1) {
+//					if (k <= 20) {
+//						r.setFill(Color.CHARTREUSE);
+//					} else if (k >= aimArray.length - 20) {
+//						r.setFill(Color.CHARTREUSE);
+//					} else {
+//						r.setFill(Color.RED);
+//					}
+//				} else {
+//					r.setFill(Color.DARKGRAY);
+//				}
+//				root.getChildren().add(r);
+//				numOfRt++;
+//
+//			}
+//		}));
+//		action.setCycleCount(Timeline.INDEFINITE);
+//		action.play();
+//	});
+//	btn3.setOnAction(e -> {
+//		action.stop();
+//	});
+//	root.setOnKeyPressed(e -> {
+//		// 退
+//		switch (e.getCode()) {
+//		case ENTER: {
+//			map.count();
+//			System.out.println("OK!");
+//			System.exit(0);
+//			break;
+//		}
+//		default:
+//			break;
+//		}
+//	});
+//
+//	primaryStage.setScene(scene);
+//	primaryStage.show();
+//}
--- src/test/Main.java
+++ src/test/Main.java
@@ -3,13 +3,22 @@ package test;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -20,130 +29,188 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 
 //extends Application
-
 public class Main extends Application {
 	Scanner in = new Scanner(System.in);
 	GoMap map = new GoMap();
-	/**
-	 * f=1:697,1497 f=2:247,473 f=3:109,209 1.stack 2.queue 3.recur 1.stack 2.staNY
-	 * 3.back beginX beginY endX endY
-	 */
-	int k = 0, f = 2, numOfRt = 0;// 数组队列下标，放大倍率
 	Timeline action;
+	int k = 0, f = 2;// 数组队列下标，放大倍率
 	int[] rdm = new int[4];
-	int x = 37;
-	int y = 73;
-	int xs = 0;
-	int ys = 50;
+	int winX = 1900, winY = 1000;
+	int x = 33, y = 23;
 
-	void draw(Pane root) {
-		numOfRt = 0;
-		for (int i = 0; i < x * f; i += f) {
-			for (int j = 0; j < y * f; j += f) {
-				Rectangle r = new Rectangle();
-				r.setX(j * f + xs);
-				r.setY(i * f + ys);
-				r.setWidth(f * f);
-				r.setHeight(f * f);
-				if (map.trans()[i / f][j / f] == 0) {
-					r.setFill(Color.BLACK);
-				} else {
-					r.setFill(Color.WHITE);
+	void draw(GridPane root, int xs, int ys) {
+		try {
+			for (int i = 0; i < x * f; i += f) {
+				for (int j = 0; j < y * f; j += f) {
+					Rectangle r = new Rectangle();
+					r.setTranslateX(j * f + xs);
+					r.setTranslateY(i * f + ys);
+					r.setWidth(f * f);
+					r.setHeight(f * f);
+					if (map.trans()[i / f][j / f] == 0) {
+						r.setFill(Color.BLACK);
+					} else {
+						r.setFill(Color.WHITE);
+					}
+					root.getChildren().add(r);
 				}
-				root.getChildren().add(r);
-				numOfRt++;
 			}
+		} catch (NullPointerException e) {
+			System.out.println("fuck");
 		}
 	}
 
-	void remove(Pane root) {
-		root.getChildren().remove(5, 5 + numOfRt);
+	void remove(GridPane root) {
+		root.getChildren().clear();
+	}
+
+	void animo(GridPane root, int xs, int ys, Object[] aimArray) {
+		action = new Timeline(new KeyFrame(Duration.millis(1), e1 -> {
+			if (k >= aimArray.length) {
+				// TODO
+				k = 0;
+				action.stop();
+			}
+			int n = 0;
+			while (n++ < 1) {
+				Rectangle r = new Rectangle();
+				int y1 = ((int) (aimArray[k++]) * f * f);
+				int x1 = ((int) (aimArray[k++]) * f * f);
+				r.setTranslateX(x1 + xs);
+				r.setTranslateY(y1 + ys);
+				r.setWidth(f * f);
+				r.setHeight(f * f);
+				if (map.trans()[y1 / (f * f)][x1 / (f * f)] == 1) {
+					if (k <= 20) {
+						r.setFill(Color.CHARTREUSE);
+					} else if (k >= aimArray.length - 20) {
+						r.setFill(Color.CHARTREUSE);
+					} else {
+						r.setFill(Color.RED);
+					}
+				} else {
+					r.setFill(Color.DARKGRAY);
+				}
+				root.getChildren().add(r);
+			}
+		}));
+		action.setCycleCount(Timeline.INDEFINITE);
+		action.play();
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
+		BorderPane root0 = new BorderPane();
+		GridPane root1 = new GridPane();
+		GridPane root2 = new GridPane();
+		GridPane root3 = new GridPane();
+		root0.setTop(root1);
+		root0.setLeft(root2);
+		root0.setRight(root3);
 
-		Pane root = new Pane();
-
+		Button btn4 = new Button("X++");
+		Button btn5 = new Button("X--");
+		Button btn6 = new Button("Y++");
+		Button btn7 = new Button("Y--");
+		ChoiceBox<String> cb1 = new ChoiceBox<String>(
+				FXCollections.observableArrayList("深度优先算法", "广度优先算法", "回溯法", "NULL"));
 		Button btn1 = new Button("FRESH");
-		Button btn2 = new Button("START");
-		Button btn3 = new Button("STOP");
-		ChoiceBox<String> cb1 = new ChoiceBox<String>(FXCollections.observableArrayList("深度优先算法", "广度优先算法", "回溯法"));
-		ChoiceBox<String> cb2 = new ChoiceBox<String>(FXCollections.observableArrayList("深度优先算法优化版", "深度优先算法", "回溯法"));
-
+		Button btn2 = new Button("START1");//优化版
+		Button btn3 = new Button("START2");//未优化版
 		rdm[0] = (int) (Math.random() * (x - 5)) + 2;
 		rdm[2] = (int) (Math.random() * (x - 5)) + 2;
 		rdm[1] = (int) (Math.random() * (y - 5)) + 2;
 		rdm[3] = (int) (Math.random() * (y - 5)) + 2;
 
-		map.setPoint(2, 2, x - 3, y - 3);
+		btn4.setTranslateX(0);
+		btn5.setTranslateX(60);
+		btn6.setTranslateX(120);
+		btn7.setTranslateX(180);
+		cb1.setTranslateX(280);
+		btn1.setTranslateX(480);
+		btn2.setTranslateX(580);
+		btn3.setTranslateX(680);
+		root1.getChildren().add(btn4);
+		root1.getChildren().add(btn5);
+		root1.getChildren().add(btn6);
+		root1.getChildren().add(btn7);
+		root1.getChildren().add(cb1);
+		root1.getChildren().add(btn1);
+		root1.getChildren().add(btn2);
+		root1.getChildren().add(btn3);
 
-		cb1.setLayoutX(0);
-		cb1.setLayoutY(0);
-		cb2.setLayoutX(150);
-		cb2.setLayoutY(0);
-		btn1.setLayoutX(350);
-		btn1.setLayoutY(0);
-		btn2.setLayoutX(570);
-		btn2.setLayoutY(0);
-		btn3.setLayoutX(650);
-		btn3.setLayoutY(0);
-		root.getChildren().add(cb1);
-		root.getChildren().add(cb2);
-		root.getChildren().add(btn1);
-		root.getChildren().add(btn2);
-		root.getChildren().add(btn3);
-		Scene scene = new Scene(root, 1900, 1000, Color.WHITE);//
+		Scene scene = new Scene(root0, winX, winY, Color.WHITE);//
+
+		map.setMap(x, y);
+		map.setPoint(2, 2, x - 3, y - 3);
+		this.draw(root2, 0, 25);
+		this.draw(root3, -y * f * f, 25);
+		btn4.setOnAction(e -> {
+			this.remove(root2);
+			this.remove(root3);
+			y += 20;
+			map.setMap(x, y);
+			map.setPoint(2, 2, x - 3, y - 3);
+			this.draw(root2, 0, 25);
+			this.draw(root3, -y * f * f, 25);
+		});
+		btn5.setOnAction(e -> {
+			this.remove(root2);
+			this.remove(root3);
+			y -= 20;
+			map.setMap(x, y);
+			map.setPoint(2, 2, x - 3, y - 3);
+			this.draw(root2, 0, 25);
+			this.draw(root3, -y * f * f, 25);
+		});
+		btn6.setOnAction(e -> {
+			this.remove(root2);
+			this.remove(root3);
+			x += 20;
+			map.setMap(x, y);
+			map.setPoint(2, 2, x - 3, y - 3);
+			this.draw(root2, 0, 25);
+			this.draw(root3, -y * f * f, 25);
+		});
+		btn7.setOnAction(e -> {
+			this.remove(root2);
+			this.remove(root3);
+			x -= 20;
+			map.setMap(x, y);
+			map.setPoint(2, 2, x - 3, y - 3);
+			this.draw(root2, 0, 25);
+			this.draw(root3, -y * f * f, 25);
+		});
 
 		cb1.getSelectionModel().selectedIndexProperty()
 				.addListener((ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
 					switch (new_val.intValue()) {
 					case 0: {
-						this.remove(root);
 						map.setMap(x, y);
+						this.remove(root2);
+						this.remove(root3);
 						map.stack();
-						this.draw(root);
+						this.draw(root2, 0, 25);
+						this.draw(root3, -y * f * f, 25);
 						break;
 					}
 					case 1: {
-						this.remove(root);
 						map.setMap(x, y);
+						this.remove(root2);
+						this.remove(root3);
 						map.queue();
-						this.draw(root);
+						this.draw(root2, 0, 25);
+						this.draw(root3, -y * f * f, 25);
 						break;
 					}
 					case 2: {
-						this.remove(root);
-						map.setMap(x, y);
 						try {
+							map.setMap(x, y);
+							this.remove(root2);
+							this.remove(root3);
 							map.recur();
-						} catch (StackOverflowError e) {
-							System.out.println("StackOverflow");
-							break;
-						}
-						this.draw(root);
-						break;
-					}
-					default:
-						System.out.println("FuckYou");
-					}
-
-				});
-
-		cb2.getSelectionModel().selectedIndexProperty()
-				.addListener((ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
-					switch (new_val.intValue()) {
-					case 0: {
-						map.runStack();
-						break;
-					}
-					case 1: {
-						map.runStackNY();
-						break;
-					}
-					case 2: {
-						try {
-							map.runBack();
+							this.draw(root2, 0, 25);
+							this.draw(root3, -y * f * f, 25);
 						} catch (StackOverflowError e) {
 							System.out.println("StackOverflow");
 							break;
@@ -151,68 +218,31 @@ public class Main extends Application {
 						break;
 					}
 					default:
-						System.out.println("FuckYou");
+						map.setMap(x, y);
 					}
+
 				});
 
 		btn1.setOnAction(e -> {
-			this.remove(root);
-			this.draw(root);
+			this.remove(root2);
+			this.remove(root3);
+			this.draw(root2, 0, 25);
+			this.draw(root3, -y * f * f, 25);
 		});
 		btn2.setOnAction(e -> {
-			this.remove(root);
-			this.draw(root);
-			// 走
-			k = 0;
-			Object[] aimArray = map.queue.toArray().clone();
-			action = new Timeline(new KeyFrame(Duration.millis(1), e1 -> {
-				if (k >= aimArray.length) {
-					k = 0;
-					action.stop();
-				}
-				int n = 0;
-				while (n++ < 1) {
-					Rectangle r = new Rectangle();
-					int y1 = ((int) (aimArray[k++]) * f * f);
-					int x1 = ((int) (aimArray[k++]) * f * f);
-					r.setX(x1 + xs);
-					r.setY(y1 + ys);
-					r.setWidth(f * f);
-					r.setHeight(f * f);
-					if (map.trans()[y1 / (f * f)][x1 / (f * f)] == 1) {
-						if (k <= 20) {
-							r.setFill(Color.CHARTREUSE);
-						} else if (k >= aimArray.length - 20) {
-							r.setFill(Color.CHARTREUSE);
-						} else {
-							r.setFill(Color.RED);
-						}
-					} else {
-						r.setFill(Color.DARKGRAY);
-					}
-					root.getChildren().add(r);
-					numOfRt++;
+			this.remove(root2);
+			this.draw(root2, 0, 25);
+			map.runStack();
+			Object[] aimArray1 = map.queue.toArray().clone();
+			this.animo(root2, 0, 25, aimArray1);
 
-				}
-			}));
-			action.setCycleCount(Timeline.INDEFINITE);
-			action.play();
 		});
 		btn3.setOnAction(e -> {
-			action.stop();
-		});
-		root.setOnKeyPressed(e -> {
-			// 退
-			switch (e.getCode()) {
-			case ENTER: {
-				map.count();
-				System.out.println("OK!");
-				System.exit(0);
-				break;
-			}
-			default:
-				break;
-			}
+			this.remove(root3);
+			this.draw(root3, -y * f * f, 25);
+			map.runStackNY();
+			Object[] aimArray2 = map.queue.toArray().clone();
+			this.animo(root3, -y * f * f, 25, aimArray2);
 		});
 
 		primaryStage.setScene(scene);
@@ -221,15 +251,5 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-//		Scanner in = new Scanner(System.in);
-//		GoMap map = new GoMap();
-//		int i = 27;
-//		int j = 97;
-//		map.setMap(i, j);
-//		/**
-//		 * 1.stack 2.queue 3.recur 1.stack 2.staNY 3.back beginX beginY endX endY
-//		 */
-//		map.choice(1, 1,2,2,i-3,j-3);
-//		map.print();
 	}
 }
